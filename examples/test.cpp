15fff7a2b2ca4424b49c39ce3e4486febf9d523c
Basil Zemann <bzemann@student.ethz.ch>
2025-12-20 12:31:50 +0100

    updated eigen from version 3 to 5

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,9 +1,14 @@
+#include <Eigen/Core>
 #include <cstddef>
 #include <iostream>
 #include <pp_ader/fv_core/concepts.hpp>
 #include <pp_ader/fv_core/mesh1d.hpp>
+#include <pp_ader/time_integrator/fvm_ssp_rk.hpp>
+#include <pp_ader/types.hpp>
+#include <pp_ader/utils.hpp>
 
 using namespace PpADER::FvCore;
+using namespace PpADER::TimeIntegrator;
 
 int main() {
   Mesh1d mesh = Mesh1d::make(0.0, 1.0, 4, 2, 2);
@@ -20,6 +25,13 @@ int main() {
   for (std::size_t i = 0; i < length; i++) {
     std::cout << "i: " << i << "x: " << MeshAPI::cell_center(mesh, i) << "\n";
   }
+
+  Eigen::VectorXd a = Eigen::VectorXd::Random(3);
+  Eigen::VectorXd b = Eigen::VectorXd::Random(3);
+
+  Eigen::VectorXd c = add(a, b);
+
+  PpADER::Utils::print(c);
 
   return 0;
 }
